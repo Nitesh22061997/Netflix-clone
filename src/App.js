f1cb09443e0021bcd4035cd7acd3689b0ed6a15b
nitesh22061997@gmail.com <163007323+Nitesh22061997@users.noreply.github.com>
2024-07-06 14:37:12 +0530

    Header,Login and store creation done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Body from "./components/Body"
 import './App.css';
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div className="App">
       <Body />
+      <Toaster />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React from 'react'
 import { IoIosArrowDropdown } from "react-icons/io";
+import { useSelector, useDispatch } from "react-redux"
+import { API_END_POINT } from '../utils/constant';
+import axios from "axios";
+import { setUser } from '../redux/userSlice';
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import { setToggle } from '../redux/movieSlice';
 
+const Header = () => {
+    const user = useSelector((store) => store.app.user);
+    const toggle = useSelector(store => store.movie.toggle);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-function Header() {
+    const logoutHandler = async () => {
+        try {
+            const res = await axios.get(`${API_END_POINT}/logout`);
+            if (res.data.success) {
+                toast.success(res.data.message);
+            }
+            dispatch(setUser(null));
+            navigate("/");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const toggleHandler = () => {
+        dispatch(setToggle());
+    }
+
     return (
-        <div className='absolute z-10 flex h-14 px-6 w-[100%] items-center justify-between bg-gradient-to-b from-black'>
-            <img className='w-56' src="https://images.ctfassets.net/y2ske730sjqp/6bhPChRFLRxc17sR8jgKbe/6fa1c6e6f37acdc97ff635cf16ba6fb3/Logos-Readability-Netflix-logo.png" alt="netflix-logo" />
-            <div className='flex items-center'>
-                < IoIosArrowDropdown size="20px" className='text-white' />
-                <h1 className='text-xl font-medium text-white'>Nitesh MERN stack</h1>
-                <div className='ml-4'>
-                    <button className='bg-red-800 text-white px-4 py-2 '>Logout</button>
-                    <button className='bg-red-800 text-white px-4 py-2 ml-2'>Search Movie</button>
-                </div>
-            </div>
+        <div className='absolute z-10 flex w-full items-center justify-between px-6 bg-gradient-to-b from-black'>
+            <img className='w-56' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1198px-Netflix_2015_logo.svg.png" alt="netflix-logo" />
+            {
+                user && (
+                    <div className='flex items-center'>
+                        <IoIosArrowDropdown size="24px" color='white' />
+                        <h1 className='text-lg font-medium text-white'>{user.fullName}</h1>
+                        <div className='ml-4'>
+                            <button onClick={logoutHandler} className='bg-red-800 text-white px-4 py-2'>Logout</button>
+                            <button onClick={toggleHandler} className='bg-red-800 text-white px-4 py-2 ml-2'>{toggle ? "Home" : "Search Movie"}</button>
+                        </div>
+                    </div>
+                )
+            }
+
         </div>
     )
 }
